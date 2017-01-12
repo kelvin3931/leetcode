52c333be69d2c3e91e50eaaa67c7ad60c78bfaab
jerrywu <Jerry_Wu@quantatw.com>
2017-01-12 10:24:14 +0800

    add countPrimes

---
+++ c/countPrimes.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int countPrimes(int n) {
+    int *isPrime = malloc(sizeof(int) * n);
+    int i;
+    int j;
+    for(i = 2; i < n; i++){
+        isPrime[i] = 1;
+    }
+
+    for(i = 2; i*i < n; i++){
+        if (!isPrime[i])
+            continue;
+        for(j = i * i; j < n; j += i){
+            isPrime[j] = 0;
+       }
+    }
+    int count = 0;
+    for (i = 2; i < n; i++) {
+        if (isPrime[i]) 
+           count++;
+    }
+    return count;
+}
+
+int main() {
+    
+    printf("%d", countPrimes(1500000));
+    return 0;
+}
